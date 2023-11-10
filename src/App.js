f0ac193f3bbdc453d70ff93477e7b13a635b9dcb
Sergio Havryliuk <havryliuk.serhii@gmail.com>
2023-11-10 21:24:12 +0200

    add axios

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import { Counter } from './features/counter/Counter';
 import { Button } from 'react-bootstrap';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const onClickUsers = () => {
+    axios
+        .get("https://jsonplaceholder.typicode.com/users")
+        .then((res) => {
+          console.log(res.data);
+        })
+        .catch((err) => console.log(err));
+  };
   return (
     <div className="App">
       <header className="App-header">
         <Counter />
-        <Button variant="primary">Primary</Button>
+        <Button variant="primary" onClick={onClickUsers}>Get users</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.

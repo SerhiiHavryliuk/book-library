f007cdf38c9a0e7c05b4070d35f3f6fd56ad5166
Sergio Havryliuk <havryliuk.serhii@gmail.com>
2023-11-13 17:08:15 +0200

    add message for empty list

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -23,27 +23,31 @@ function BookList() {
   return (
     <div className={style.bookList}>
       <h2> Book List </h2>
-      <ul className={style.list}>
-        {allBooks.map((item, index) => {
-          return (
-            <li key={index} className={style.item}>
-              <div className={style.listDescription}>
-                {item.title} <b> {item.author}</b>
-              </div>
-              <div onClick={() => handleToggleFavoriteBook(item.id)}>
-                {item.isFavorite ? <BsBookmarkCheckFill /> : <BsBookmarkCheck />}
-                <Button
-                  variant="outline-danger"
-                  size="sm"
-                  className={style.buttonDelete}
-                  onClick={() => handleDeleteBook(item.id)}>
-                  Delete
-                </Button>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+      {allBooks.length ? (
+        <ul className={style.list}>
+          {allBooks.map((item, index) => {
+            return (
+              <li key={index} className={style.item}>
+                <div className={style.listDescription}>
+                  {item.title} <b> {item.author}</b>
+                </div>
+                <div onClick={() => handleToggleFavoriteBook(item.id)}>
+                  {item.isFavorite ? <BsBookmarkCheckFill /> : <BsBookmarkCheck />}
+                  <Button
+                    variant="outline-danger"
+                    size="sm"
+                    className={style.buttonDelete}
+                    onClick={() => handleDeleteBook(item.id)}>
+                    Delete
+                  </Button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <p> List is empty!</p>
+      )}
     </div>
   );
 }
